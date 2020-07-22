01d149f315553dc8ad9175478bedd4570eda2ae2
Shae Allison <sallison@ADKs-MacBook-Pro-9.local>
2020-07-22 18:07:51 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'antd';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    Supersearch Application
+    <br/>
+    <Button type="primary">Example Button</Button>
+  </div>
+);
 
 export default App;
